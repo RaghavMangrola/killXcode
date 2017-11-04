dd4eef7f3c8d1d16df7ad3db54dcab2c1670b5a3
Raghav Mangrola <raghav.mangrola@gmail.com>
2017-11-04 00:40:10 -0700

    Initial Commit

---
+++ XcodeKilla/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XcodeKilla
+//
+//  Created by Raghav Mangrola on 11/4/17.
+//  Copyright © 2017 Raghav Mangrola. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
